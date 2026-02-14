8d67fe0807161d5ad79b840ea60aeaa62461403c
Link4Electronics <link4electronics@gmail.com>
2026-02-14 15:16:24 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/nugget-doom.png
 export DESKTOP=/usr/share/applications/io.github.MrAlaux.Nugget-Doom.desktop
+export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
