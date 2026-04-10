d813b55cef78c1a14247962bc0526edb2f277d32
Link4Electronics <link4electronics@gmail.com>
2026-04-10 00:49:38 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,10 +3,9 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q nugget-doom | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/nugget-doom.png
 export DESKTOP=/usr/share/applications/io.github.MrAlaux.Nugget-Doom.desktop
@@ -14,13 +13,11 @@ export STARTUPWMCLASS=nugget-doom
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/nugget-doom /usr/bin/nugget-doom-setup
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/nugget-doom /usr/bin/nugget-doom-setup /usr/lib/libfluidsynth.so* /usr/lib/libopenal.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
