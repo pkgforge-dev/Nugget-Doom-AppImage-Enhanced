0216d7539ca1f37169e6d8d80b849a0564b2742b
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:43:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,16 +7,25 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake          \
-    fluidsynth     \
-    sdl2           \
-    sdl2_net
+     cmake              \
+     fluidsynth         \
+     hicolor-icon-theme \
+     libebur128         \
+     openal             \
+     sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package nugget-doom
+echo "Building stable version of Nugget Doom..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/MrAlaux/Nugget-Doom"
+TAG="$(curl -s https://api.github.com/repos/MrAlaux/Nugget-Doom/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)"
+VERSION="$(echo "$TAG" | sed 's/^nugget-doom-//')"
+git clone --depth 1 --branch "$TAG" "$REPO" ./Nugget-Doom
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+cmake -S ./Nugget-Doom -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build build -j$(nproc)
+cmake --install build
